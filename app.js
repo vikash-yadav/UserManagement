165cc8f79990a13989ce836edd9446f1fc2c53dc
vikash-yadav <vikash.yadav9911@gmail.com>
2021-07-25 21:19:18 +0530

    Initail Commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 const { config } = require("process");
 
 const app = express();
+
+const staticPath = path.join(__dirname,"../public");
+app.use(express.static(staticPath));
 
 const PORT = process.env.PORT || 3000;
 
