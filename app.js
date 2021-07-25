cd693aab4217855d5d618397e99a3d9b3bc48945
vikash-yadav <vikash.yadav9911@gmail.com>
2021-07-26 03:01:44 +0530

    Inital Commit

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const { config } = require("process");
 
 const app = express();
 
-const staticPath = path.join(__dirname,"../public");
+const staticPath = path.join(__dirname, "../public");
 app.use(express.static(staticPath));
+
 
 const PORT = process.env.PORT || 3000;
 
-app.get('',(req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-})
-
+app.get("", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 app.listen(PORT, () => {
   console.log("Server us running on http://localhost:$(PORT)");
